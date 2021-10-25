06fc18df71a05a6c945875d819b6d4f17758173b
Sergey Chivirda <si.chivirda@gmail.com>
2021-10-26 01:23:54 +0300

    feat: Включил отображение ошибок. Поправил пути. Переместил приложение в папку public.

--- pages/hello.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '../vendor/autoload.php';
-
-$name = $request->get("name", "World");
-$response->setContent(sprintf("Hello, %s", htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
