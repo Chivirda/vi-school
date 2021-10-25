9977e88c0c140e1da46ef26bb331df44eca06eaf
Sergey Chivirda <si.chivirda@gmail.com>
2021-10-25 22:40:24 +0300

    feat: Реализовал вывод приветствия из адресной строки

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+require_once './vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get("name", "World");
+
+$response = new Response(sprintf('Hello, %s!', ucfirst(htmlspecialchars($input, ENT_QUOTES, 'UTF-8'))));
+
+$response->send();
+
