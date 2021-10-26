8d30a218d4c4235cf66ae6e52f60bcd8f7e099a8
Sergey Chivirda <si.chivirda@gmail.com>
2021-10-26 22:21:21 +0300

    refactor: Выделил создание роутов в файл app.php

--- public/index.php
+++ public/index.php
@@ -4,27 +4,22 @@ require_once '../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = include './app.php';
 
 $context = new RequestContext();
 $context->fromRequest($request);
-
-$routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
-
 $matcher = new UrlMatcher($routes, $context);
 
 try {
     extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
     include sprintf('./pages/%s.php', $_route);
+
+    $response = new Response(ob_get_clean());
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Страница не существует', 404);
 } catch (Exception $exception) {
