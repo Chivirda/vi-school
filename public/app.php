b52a81fd63f88212b22451e9dd6581aa8c8dc5ed
Sergey Chivirda <si.chivirda@gmail.com>
2021-10-27 23:26:33 +0300

    feat: Добавил проверку на високосный год

--- public/app.php
+++ public/app.php
@@ -1,6 +1,17 @@
 <?php
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+function is_leap_year($year = null)
+{
+  if ($year === null) {
+    $year = date('Y');
+  }
+
+  return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
 
 $routes = new RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', [
@@ -8,5 +19,16 @@ $routes->add('hello', new Route('/hello/{name}', [
   '_controller' => 'render_template'
 ]));
 $routes->add('bye', new Route('/bye'));
+$routes->add('leap_year', new Route('/is_leap_year/{year}', [
+  'year' => null,
+  '_controller' => function (Request $request): Response
+  {
+    if (is_leap_year($request->attributes->get('year'))) {
+      return new Response('Yep, this is a leap year!');
+    }
+
+    return new Response('Nope, this is not leap year.');
+  }
+]));
 
 return $routes;
